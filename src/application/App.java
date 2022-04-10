241e6c5b97443d9ca19c4386883739679cfa64ad
qrhassan <103136385+qrhassan@users.noreply.github.com>
2022-04-10 15:41:37 -0400

    updated

---
+++ src/application/App.java
@@ -0,0 +1,25 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		int dogs=8;
+		int cats=3;
+		System.out.println("1.Number of Dogs are:" + dogs);
+		System.out.println("1.Number of Cats are:" + cats);
+
+		dogs=cats;
+		cats=dogs;
+		cats=10;
+		System.out.println("2.Number of Dogs are:" + dogs);
+		System.out.println("2.Number of Cats are:" + cats);
+
+		
+		int length = 25;
+		
+		
+		
+		
+	}
+
+}
